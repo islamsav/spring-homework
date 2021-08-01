54e2df4275573bc03f86bee5fbf2a04ff17fde33
Islam Savzikhanov <savzikhanov@lanit.ru>
2021-08-02 00:16:21 +0300

    2.5 Spring MVC - основы работы с базой данных

--- src/main/java/org/example/WebAppInitializer.java
+++ src/main/java/org/example/WebAppInitializer.java
@@ -3,6 +3,7 @@ package org.example;
 import org.apache.log4j.Logger;
 import org.example.app.config.AppContextConfig;
 import org.example.web.config.WebContextConfig;
+import org.h2.server.web.WebServlet;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -19,8 +20,6 @@ public class WebAppInitializer implements WebApplicationInitializer {
     public void onStartup(javax.servlet.ServletContext servletContext) throws ServletException {
 
         logger.info("loading app config");
-//        XmlWebApplicationContext appContext = new XmlWebApplicationContext();
-//        appContext.setConfigLocation("classpath:app-config.xml");
         AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
         appContext.register(AppContextConfig.class);
         servletContext.addListener(new ContextLoaderListener(appContext));
@@ -35,5 +34,9 @@ public class WebAppInitializer implements WebApplicationInitializer {
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
         logger.info("dispatcher ready");
+
+        ServletRegistration.Dynamic servletRegistration = servletContext.addServlet("h2-console", new WebServlet());
+        servletRegistration.setLoadOnStartup(2);
+        servletRegistration.addMapping("/console/*");
     }
 }
