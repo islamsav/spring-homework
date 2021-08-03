11a674bcc16b15eab6088114117c0a2d2be64a70
Islam Savzikhanov <savzikhanov@lanit.ru>
2021-08-04 00:31:10 +0300

    Настроить валидацию всех полей ввода в приложении. 🦾

--- src/main/java/org/example/app/config/AppSecurityConfig.java
+++ src/main/java/org/example/app/config/AppSecurityConfig.java
@@ -35,7 +35,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/login*").permitAll()
+                .antMatchers("/login/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
--- src/main/java/org/example/web/dto/Book.java
+++ src/main/java/org/example/web/dto/Book.java
@@ -1,12 +1,17 @@
 package org.example.web.dto;
 
 import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public class Book {
 
     private Integer id;
+    @Size(min = 1, max = 250)
     private String author;
+    @Size(min = 1, max = 250)
     private String title;
+    @NotNull
     @Digits(integer = 4, fraction = 0)
     private Integer size;
 
