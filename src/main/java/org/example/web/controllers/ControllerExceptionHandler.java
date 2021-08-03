4f23b01238dbb989418ce01c7aa4fabd5867bb8c
Islam Savzikhanov <savzikhanov@lanit.ru>
2021-08-04 00:45:11 +0300

    Создать обработчик ошибки, возникающей при попытке загрузить на сервер null (не указав предварительно в диалоговом окне загружаемый файл). 🦾🦾

---
+++ src/main/java/org/example/web/controllers/ControllerExceptionHandler.java
@@ -0,0 +1,16 @@
+package org.example.web.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public String notFoundHandler() {
+        return "errors/500";
+    }
+}
