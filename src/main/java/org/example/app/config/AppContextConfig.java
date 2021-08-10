373af307826cc3c1dbc3d420685ce11b148f378d
Islam Savzikhanov <savzikhanov@lanit.ru>
2021-08-10 15:23:08 +0400

    Правка README.md

--- src/main/java/org/example/app/config/AppContextConfig.java
+++
@@ -1,9 +0,0 @@
-package org.example.app.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "org.example.app")
-public class AppContextConfig {
-}
--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO books(author, title, size) VALUES ('Dostoevsky', 'Idiot', 350);
-INSERT INTO books(author, title, size) VALUES ('Dostoevsky', 'Belie nochi', 500);
-INSERT INTO books(author, title, size) VALUES ('Dostoevsky', 'Zapiski', 650);
-INSERT INTO books(author, title, size) VALUES ('Dostoevsky', 'Krokodil', 200);
-INSERT INTO books(author, title, size) VALUES ('Pushkin', 'Ulukomorie', 230);
-INSERT INTO books(author, title, size) VALUES ('Pushkin', 'Ruslan and Ludmila', 23);
-INSERT INTO books(author, title, size) VALUES ('Pushkin', 'Orel', 600);
