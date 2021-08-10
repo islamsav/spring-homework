db6b9672f584065a64d2af2adec32fa2923335bf
Islam Savzikhanov <savzikhanov@lanit.ru>
2021-08-10 19:01:25 +0400

    Выполнил 3 модуль без ДЗ

---
+++ src/main/java/com/example/MyBookShopApp/dto/Book.java
@@ -0,0 +1,13 @@
+package com.example.MyBookShopApp.dto;
+
+import lombok.Data;
+
+@Data
+public class Book {
+
+    private Integer id;
+    private String author;
+    private String title;
+    private String priceOld;
+    private String price;
+}
