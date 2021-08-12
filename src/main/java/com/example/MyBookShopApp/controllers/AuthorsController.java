85baf5fe8a2f989ab9ad4bf37d9f43ea2c18933a
savzikhanov@lanit.ru <Imahk$74364>
2021-08-12 22:54:52 +0400

    4.4 Standart Dialect - Циклы, Условия

---
+++ src/main/java/com/example/MyBookShopApp/controllers/AuthorsController.java
@@ -0,0 +1,32 @@
+package com.example.MyBookShopApp.controllers;
+
+import com.example.MyBookShopApp.service.AuthorsService;
+import com.example.MyBookShopApp.dto.Author;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class AuthorsController {
+
+    private final AuthorsService authorsService;
+
+    @Autowired
+    public AuthorsController(AuthorsService authorsService) {
+        this.authorsService = authorsService;
+    }
+
+    @ModelAttribute("authorsMap")
+    public Map<String, List<Author>> authorsMap() {
+        return authorsService.getAuthorsMap();
+    }
+
+    @GetMapping("/authors")
+    public String authorsPage() {
+        return "/authors/index";
+    }
+}
--- src/main/java/com/example/MyBookShopApp/controllers/MainPageController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/MainPageController.java
@@ -1,16 +1,15 @@
 package com.example.MyBookShopApp.controllers;
 
-import com.example.MyBookShopApp.data.BookService;
+import com.example.MyBookShopApp.service.BookService;
+import com.example.MyBookShopApp.dto.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
-import java.util.Date;
+import java.util.List;
 
 @Controller
-@RequestMapping("/bookshop")
 public class MainPageController {
 
     private final BookService bookService;
@@ -20,24 +19,13 @@ public class MainPageController {
         this.bookService = bookService;
     }
 
-    @GetMapping("/main")
-    public String mainPage(Model model) {
-        model.addAttribute("bookData", bookService.getBooksData());
-        model.addAttribute("searchPlaceholder", "new search placeholder");
-        model.addAttribute("serverTime", new Date());
-        model.addAttribute("placeholderTextPart2", "SERVER");
-        model.addAttribute("messageTemplate", "searchbar.placeholder2");
+    @ModelAttribute("recommendedBooks")
+    public List<Book> recommendedBooks() {
+        return bookService.getBooksData();
+    }
+
+    @GetMapping("/")
+    public String mainPage() {
         return "index";
-    }
-
-    @GetMapping("/genres")
-    public String genresPage() {
-        return "genres/genres";
-    }
-
-    @GetMapping("/authors")
-    public String authorsPage(Model model) {
-        model.addAttribute("bookAuthors", bookService.getBooksData());
-        return "authors/authors";
     }
 }
