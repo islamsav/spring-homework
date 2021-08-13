215f1be090f058ad1295882c38e99135edfea6cf
Islam Savzikhanov <savzikhanov@lanit.ru>
2021-08-13 18:34:08 +0400

    Заменил хардкод href на вызовы

---
+++ src/main/java/com/example/MyBookShopApp/controllers/PopularController.java
@@ -0,0 +1,31 @@
+package com.example.MyBookShopApp.controllers;
+
+import com.example.MyBookShopApp.dto.Book;
+import com.example.MyBookShopApp.service.BooksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+
+@Controller
+public class PopularController {
+
+    private final BooksService booksService;
+
+    @Autowired
+    public PopularController(BooksService booksService) {
+        this.booksService = booksService;
+    }
+
+    @ModelAttribute("bookList")
+    public List<Book> bookList() {
+        return booksService.getAllBooks();
+    }
+
+    @GetMapping("/books/popular")
+    public String popularPage() {
+        return "books/popular";
+    }
+}
---
+++ src/main/java/com/example/MyBookShopApp/controllers/RecentController.java
@@ -0,0 +1,34 @@
+package com.example.MyBookShopApp.controllers;
+
+import com.example.MyBookShopApp.dto.Book;
+import com.example.MyBookShopApp.service.BooksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
+
+/**
+ * Новинки
+ */
+@Controller
+public class RecentController {
+
+    private final BooksService booksService;
+
+    @Autowired
+    public RecentController(BooksService booksService) {
+        this.booksService = booksService;
+    }
+
+    @GetMapping("/books/recent")
+    public String newItemsPage() {
+        return "books/recent";
+    }
+
+    @ModelAttribute("bookList")
+    public List<Book> bookList() {
+        return booksService.getAllBooks();
+    }
+}
