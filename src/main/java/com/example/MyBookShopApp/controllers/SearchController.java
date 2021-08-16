aa3f39826909fc1c320ac22ac92e67d27bf9e43f
Islam Savzikhanov <savzikhanov@lanit.ru>
2021-08-16 13:56:32 +0400

    Локализация контента

--- src/main/java/com/example/MyBookShopApp/controllers/SearchController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/SearchController.java
@@ -1,10 +1,28 @@
 package com.example.MyBookShopApp.controllers;
 
+import com.example.MyBookShopApp.dto.Book;
+import com.example.MyBookShopApp.service.BooksService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.List;
 
 @Controller
 public class SearchController {
+
+    private final BooksService booksService;
+
+    @Autowired
+    public SearchController(BooksService booksService) {
+        this.booksService = booksService;
+    }
+
+    @ModelAttribute("bookList")
+    public List<Book> bookList() {
+        return booksService.getAllBooks();
+    }
 
     @GetMapping("/search")
     public String searchPage() {
