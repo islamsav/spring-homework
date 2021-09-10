bdbfe7a72077f90f7080a718192bccae06364a7a
Islam Savzikhanov <savzikhanov@lanit.ru>
2021-09-11 02:03:30 +0400

    Подключить постраничный вывод для лент «Новинки» и «Популярное» на главной странице 🦾

---
+++ src/main/java/com/example/MyBookShopApp/dto/BookDto.java
@@ -0,0 +1,24 @@
+package com.example.MyBookShopApp.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@ToString
+@Builder
+public class BookDto {
+    private Integer id;
+    private String description;
+    private Integer discount;
+    private String image;
+    private Boolean isBestseller;
+    private Integer price;
+    private LocalDate pubDate;
+    private String slug;
+    private String title;
+}
---
+++ src/main/java/com/example/MyBookShopApp/factory/BookDtoFactory.java
@@ -0,0 +1,42 @@
+package com.example.MyBookShopApp.factory;
+
+import com.example.MyBookShopApp.dto.BookDto;
+import com.example.MyBookShopApp.entity.book.BookEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class BookDtoFactory {
+
+    /**
+     * Метод преобразует Entity класс в DTO класс отсекая ненужные поля
+     * @param bookEntity BookEntity
+     * @return BookDto
+     */
+    public BookDto createBookDto(BookEntity bookEntity) {
+        return BookDto.builder()
+                .id(bookEntity.getId())
+                .description(bookEntity.getDescription())
+                .discount(bookEntity.getDiscount())
+                .image(bookEntity.getImage())
+                .isBestseller(bookEntity.getIsBestseller())
+                .price(bookEntity.getPrice())
+                .pubDate(bookEntity.getPubDate())
+                .slug(bookEntity.getSlug())
+                .title(bookEntity.getTitle())
+                .build();
+    }
+
+    /**
+     * Метод преобразует List<BookEntity> в List<BookDto> отсекая ненужные поля
+     * @param bookEntityList List<BookEntity>
+     * @return List<BookDto>
+     */
+    public List<BookDto> createBookDtoList(List<BookEntity> bookEntityList) {
+        return bookEntityList.stream()
+                .map(this::createBookDto)
+                .collect(Collectors.toList());
+    }
+}
