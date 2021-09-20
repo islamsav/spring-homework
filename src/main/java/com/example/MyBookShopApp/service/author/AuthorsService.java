8fd66211b13819f9513160eaf11bb398c4f39652
Islam Savzikhanov <savzikhanov@lanit.ru>
2021-09-20 19:40:48 +0400

    Завершил

--- src/main/java/com/example/MyBookShopApp/service/author/AuthorsService.java
+++ src/main/java/com/example/MyBookShopApp/service/author/AuthorsService.java
@@ -3,9 +3,6 @@ package com.example.MyBookShopApp.service.author;
 import com.example.MyBookShopApp.entity.author.AuthorEntity;
 import com.example.MyBookShopApp.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
